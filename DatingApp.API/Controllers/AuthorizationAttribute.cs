2a25e9df04251d8cbdc72fc59b7753365344e3f7
sarosh.iqbal@outlook.com <sarosh.iqbal@outlook.com>
2020-03-25 02:58:26 +0300

    Initail Commit

---
+++ DatingApp.API/Controllers/AuthorizationAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DatingApp.API.Controllers
+{
+    internal class AuthorizationAttribute : Attribute
+    {
+    }
+}
---
+++ DatingApp.API/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
